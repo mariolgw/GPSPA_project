fc30c31e0b2794d33c52fe8f2988c1dce59ae10a
SamucaXP29 <samuel.costa.cabral@gmail.com>
2025-02-05 15:57:39 +0000

    Updated DB and dbconnection file

--- database/database.sql
+++ database/database.sql
@@ -1,14 +1,26 @@
--- table that will allocate the grouped_result
-CREATE TABLE gropued_result (
-    stop_id VARCHAR(50),
-    stop_headsign VARCHAR(100),
-    arrival_time TIME
+-- Create table for stops
+CREATE TABLE stops (
+    stop_id VARCHAR(50) PRIMARY KEY,
+    stop_lat DECIMAL(9, 6),
+    stop_lon DECIMAL(9, 6)
 );
 
--- table that will allocate the stations
-CREATE TABLE stations (
+-- Create table for arrival times
+CREATE TABLE arrival_times (
     stop_id VARCHAR(50),
-    stop_name VARCHAR(100),
-    stop_lat FLOAT,
-    stop_lon FLOAT
+    stop_headsign VARCHAR(100),
+    arrival_time TIME,
+    FOREIGN KEY (stop_id) REFERENCES stops(stop_id)
+);
+
+-- Create table for station info
+CREATE TABLE station_info (
+    stop_id VARCHAR(50),
+    trip_id VARCHAR(50),
+    stop_url TEXT,
+    stop_headsign VARCHAR(100),
+    route_short_name VARCHAR(10),
+    route_long_name VARCHAR(50),
+    route_color VARCHAR(10),
+    FOREIGN KEY (stop_id) REFERENCES stops(stop_id)
 );
