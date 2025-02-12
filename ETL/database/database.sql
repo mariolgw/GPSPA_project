d1dc82ae497d85012a53c65bf4659975056d8aeb
LourencoPro <trindade.t.alexandre@gmail.com>
2025-02-12 15:26:30 +0000

    database file changed

--- ETL/database/database.sql
+++ ETL/database/database.sql
@@ -1,8 +1,10 @@
 -- Create table for stops
 CREATE TABLE stops (
     stop_id VARCHAR(50) PRIMARY KEY,
+    stop_name VARCHAR(100),
     stop_lat DECIMAL(9, 6),
-    stop_lon DECIMAL(9, 6)
+    stop_lon DECIMAL(9, 6),
+    geom geometry(POINT, 4326)
 );
 
 -- Create table for arrival times
@@ -17,7 +19,7 @@ CREATE TABLE departure_times (
 
 -- Create table for station info
 CREATE TABLE station_info (
-    stop_id VARCHAR(50),
+    stop_id VARCHAR(50) PRIMARY key,
     trip_id VARCHAR(50),
     stop_url TEXT,
     stop_headsign VARCHAR(100),
@@ -26,3 +28,19 @@ CREATE TABLE station_info (
     route_color VARCHAR(10),
     FOREIGN KEY (stop_id) REFERENCES stops(stop_id)
 );
+
+
+CREATE Or REPLACE FUNCTION create_geom()
+RETURNS TRIGGER AS
+$$
+BEGIN
+	new.geom :=	ST_SetSRID(ST_MakePoint(new.stop_lon, new.stop_lat), 4326);
+	RETURN new;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER insert_rides_in_pa
+AFTER INSERT ON stops
+FOR EACH ROW
+EXECUTE PROCEDURE create_geom();
