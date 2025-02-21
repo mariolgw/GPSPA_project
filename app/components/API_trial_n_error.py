36be316fde1dc7cc02dd9c49d9c97cc9383b504a
Mario Lloyd Galvão-Wilson <mario@Marios-MacBook-Pro.local>
2025-02-21 00:06:13 +0000

    finally got timetable API working

--- app/components/API_trial_n_error.py
+++ app/components/API_trial_n_error.py
@@ -2,20 +2,12 @@ from flask import Flask, request, jsonify
 import psycopg2
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
-from datetime import datetime, timezone, timedelta
-import pytz
-from typing import Optional, List, Dict
-import holidays
-#from flask_cors import CORS
-
-#from utils import format_geojson
-
+from datetime import datetime
 
 app = Flask(__name__)
 
 def db_connection():
     """Establish a connection to the PostgreSQL database."""
-    # connect to the postgres DB. values may change depending on your postgres setup
     conn = psycopg2.connect(
         dbname="metro",
         user="postgres",
@@ -23,43 +15,10 @@ def db_connection():
         host="localhost",
         port=5432
     )
-
     return conn
 
-#get station info
-@app.route('/station_info', methods=['GET'])
-def get_station_info():
-    """Retrieve station information from the database."""
-    stop_id = request.args.get('stop_id')
-    if not stop_id:
-        return jsonify({"error": "stop_id parameter is required"}), 400
-
-    conn = db_connection()
-    
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
-
-    # Execute a query
-    query = sql.SQL("SELECT stop_name, stop_lat, stop_lon FROM stops WHERE stop_id = %s")
-    cur.execute(query, (stop_id,))
-
-    # Retrieve query results
-    station = cur.fetchone()
-
-    if station:
-        response = jsonify(station)
-    else:
-        response = jsonify({"error": "Station not found"}), 404
-
-    # Close communication with the database
-    cur.close()
-    conn.close()
-
-    return response
-
-        
-@app.route('/next_trains', methods=['GET'])
-def next_trains():
+@app.route('/tne', methods=['GET'])
+def tne():
     """Get next 3 trains after current time."""
     stop_id = request.args.get('stop_id')
     if not stop_id:
